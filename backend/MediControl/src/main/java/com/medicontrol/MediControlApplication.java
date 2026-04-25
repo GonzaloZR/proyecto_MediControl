574b1733ebef9965fac11bbab134072f69d01f01
xDeathx1231 <gonzalol2424@gmail.com>
2026-04-25 13:10:05 -0500

    feat: configurar proyecto backend en Spring Boot con IntelliJ IDEA

---
+++ backend/MediControl/src/main/java/com/medicontrol/MediControlApplication.java
@@ -0,0 +1,13 @@
+package com.medicontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediControlApplication.class, args);
+    }
+
+}
---
+++ backend/MediControl/src/test/java/com/medicontrol/MediControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
